b2e8f681609fdda32b3b76725cdec5df70e9c636
abir2301 <abirchalghamie@gmail.com>
2022-10-17 15:53:18 -0700

    coomit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -24,8 +24,7 @@ int main(void)
 
 					printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 
-						printf("Size of a float: %i byte(s)\n", sizeof(floati));
-
+						printf("Size of a float: %i byte(s)\n", sizeof(float));
 							return (0);
 
 }
