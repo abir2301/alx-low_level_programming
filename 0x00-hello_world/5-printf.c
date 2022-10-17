71ffd036b401bf26e6b25769eaa2c5ad3c6cf6ab
abir2301 <abirchalghamie@gmail.com>
2022-10-17 15:45:33 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+/**
+ *
+ *  * main - Prints a text
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
