ac948909d58012afb4655609b86a02cbffbe4706
abir2301 <abirchalghamie@gmail.com>
2022-10-18 06:40:29 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ *
+ *  * main - prints the string "Holberton" from a character array.
+ *
+ *   * Return: 0 on success.
+ *
+ *    */
+
+int main(void)
+
+{
+
+		char c[] = "_putchar";
+
+			int i;
+
+
+
+				for (i = 0; i < 9; i++)
+
+						{
+
+									putchar(c[i]);
+
+										}
+
+					putchar('\n');
+
+						return (0);
+
+}
